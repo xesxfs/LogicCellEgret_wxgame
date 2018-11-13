3d343731806379976704e21ebc7ce3c6f12e6438
dasheng_chenwei <chenwei@dasheng.tv>
2018-11-13 18:59:55 +0800

    wx

--- platform.js
+++ platform.js
@@ -4,72 +4,113 @@
 
 class WxgamePlatform {
 
-    name = 'wxgame'
+  name = 'wxgame'
 
-    login() {
-        return new Promise((resolve, reject) => {
-            wx.login({
-                success: (res) => {
-                    resolve(res)
-                }
-            })
-        })
-    }
+  login() {
+    return new Promise((resolve, reject) => {
+      wx.login({
+        success: (res) => {
+          resolve(res)
+        }
+      })
+    })
+  }
 
-    getUserInfo() {
-        return new Promise((resolve, reject) => {
-            wx.getUserInfo({
-                withCredentials: true,
-                success: function (res) {
-                    var userInfo = res.userInfo
-                    var nickName = userInfo.nickName
-                    var avatarUrl = userInfo.avatarUrl
-                    var gender = userInfo.gender //性别 0：未知、1：男、2：女
-                    var province = userInfo.province
-                    var city = userInfo.city
-                    var country = userInfo.country
-                    resolve(userInfo);
-                }
-            })
-        })
-    }
+  getUserInfo() {
+    return new Promise((resolve, reject) => {
+      wx.getUserInfo({
+        withCredentials: true,
+        success: function(res) {
+          var userInfo = res.userInfo
+          var nickName = userInfo.nickName
+          var avatarUrl = userInfo.avatarUrl
+          var gender = userInfo.gender //性别 0：未知、1：男、2：女
+          var province = userInfo.province
+          var city = userInfo.city
+          var country = userInfo.country
+          resolve(userInfo);
+        }
+      })
+    })
+  }
 
-    openDataContext = new WxgameOpenDataContext();
+  showShareMenu(data) {
+    wx.showShareMenu({
+      withShareTicket: data.withShareTicket,
+      success: function(res) {
+        console.log(res);
+      },
+      fail: function(res) {
+        console.log(res);
+      },
+      complete: function(res) {}
+    })
+  }
+
+  onShareAppMessage(callback) {
+    wx.onShareAppMessage(callback)
+  }
+
+  shareAppMessage(data) {
+    return new Promise((resolve, reject) => {
+      wx.shareAppMessage({
+        title: data.title,
+        imageUrl: data.imageUrl,
+        query: data.query,
+        success: function(res) {
+          resolve(res);
+        },
+        fail: function(res) {
+          reject(res);
+        },
+        complete: function(res) {}
+      })
+    })
+  }
+
+  getUpdateManager() {
+    if (wx.getUpdateManager) {
+      return wx.getUpdateManager();
+    } else
+      return null;
+  }
+
+  openDataContext = new WxgameOpenDataContext();
 }
 
 class WxgameOpenDataContext {
 
-    createDisplayObject(type, width, height) {
-        const bitmapdata = new egret.BitmapData(sharedCanvas);
-        bitmapdata.$deleteSource = false;
-        const texture = new egret.Texture();
-        texture._setBitmapData(bitmapdata);
-        const bitmap = new egret.Bitmap(texture);
-        bitmap.width = width;
-        bitmap.height = height;
+  createDisplayObject(type, width, height) {
+    const bitmapdata = new egret.BitmapData(sharedCanvas);
+    bitmapdata.$deleteSource = false;
+    const texture = new egret.Texture();
+    texture._setBitmapData(bitmapdata);
+    const bitmap = new egret.Bitmap(texture);
+    bitmap.width = width;
+    bitmap.height = height;
 
-        if (egret.Capabilities.renderMode == "webgl") {
-            const renderContext = egret.wxgame.WebGLRenderContext.getInstance();
-            const context = renderContext.context;
-            ////需要用到最新的微信版本
-            ////调用其接口WebGLRenderingContext.wxBindCanvasTexture(number texture, Canvas canvas)
-            ////如果没有该接口，会进行如下处理，保证画面渲染正确，但会占用内存。
-            if (!context.wxBindCanvasTexture) {
-                egret.startTick((timeStarmp) => {
-                    egret.WebGLUtils.deleteWebGLTexture(bitmapdata.webGLTexture);
-                    bitmapdata.webGLTexture = null;
-                    return false;
-                }, this);
-            }
-        }
-        return bitmap;
+    if (egret.Capabilities.renderMode == "webgl") {
+      const renderContext = egret.wxgame.WebGLRenderContext.getInstance();
+      const context = renderContext.context;
+      ////需要用到最新的微信版本
+      ////调用其接口WebGLRenderingContext.wxBindCanvasTexture(number texture, Canvas canvas)
+      ////如果没有该接口，会进行如下处理，保证画面渲染正确，但会占用内存。
+      if (!context.wxBindCanvasTexture) {
+        egret.startTick((timeStarmp) => {
+          egret.WebGLUtils.deleteWebGLTexture(bitmapdata.webGLTexture);
+          bitmapdata.webGLTexture = null;
+          return false;
+        }, this);
+      }
     }
+    return bitmap;
+  }
 
 
-    postMessage(data) {
-        const openDataContext = wx.getOpenDataContext();
-        openDataContext.postMessage(data);
-    }
+  postMessage(data) {
+    const openDataContext = wx.getOpenDataContext();
+    openDataContext.postMessage(data);
+  }
 }
 
 
